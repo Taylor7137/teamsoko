2e9b947c63db1c37f6fa17f54f4d31d055ae7d42
Taylor Howard <taylor_howard@Taylors-MacBook-Pro.local>
2016-04-17 13:05:54 -0400

    update

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 var express = require("express"),
 	 logger = require("morgan")('dev'),
 	 moment = require('moment'),
+	 path    = require('path'),
+	 mongoose = require('mongoose'),
+	 Schema = mongoose.Schema,
+	 bodyParser = require('body-parser'),
 	 server = express();
 	 
 server.use(express.static(__dirname+'/public'));
 server.use(logger);
 server.set('port', process.env.PORT || 8080);
 
-server.get('/', frontpage);
-server.get('/weather', main)
+server.get('/', home);
+server.get('/videos', videos);
 
-server.listen(server.get('port'), listenCallback);
-
-
-function frontpage(req, res){
+function home(req, res){
   res.sendFile('/html/index.html', {root: __dirname+'/public'});
 }
 
-function main(req, res){
-	res.sendFile('/html/main.html', {root: __dirname+'/public'})
+function videos(req, res){
+	res.sendFile('/html/videos.html', {root: __dirname+'/public'})
 }
+
+server.listen(server.get('port'), listenCallback);
 
 function listenCallback(){
   console.log('Now listening on port ' + server.get('port'));
